77a98ed06d26635ddce2c369d67cef41e81bec8b
giventadevelop <giventa.develop@gmail.com>
2025-01-20 01:44:25 -0500

    laundry changes

--- google_calendar_and_contacts_automate/google_calendar_and_contacts_automate.spec
+++ google_calendar_and_contacts_automate/google_calendar_and_contacts_automate.spec
@@ -1,38 +1,70 @@
 # -*- mode: python ; coding: utf-8 -*-
 
+block_cipher = None
 
 a = Analysis(
     ['google_calendar_and_contacts_automate.py'],
-pathex=['C:/Users/gain/git/python/tasks_automation/google_calendar_and_contacts_automate'],
-binaries=[],
-datas=[('C:/Users/gain/git/python/tasks_automation/google_calendar_and_contacts_automate/property_files/calendar-automate-srvc-account-ref-file.json','property_files'), ('C:/Users/gain/git/python/tasks_automation/google_calendar_and_contacts_automate/property_files/calendar_api_properties.properties','property_files'),
-('C:/Users/gain/git/python/tasks_automation/google_calendar_and_contacts_automate/property_files/google_desktop_oauth_client_contacts_api.json','property_files'),
-('C:/Users/gain/git/python/tasks_automation/google_calendar_and_contacts_automate/property_files/token.pickle','property_files')
- ],
-hiddenimports=[],
+    pathex=[
+        'C:/Users/gain/git/python/tasks_automation/google_calendar_and_contacts_automate',
+        'C:/Users/gain/git/python/tasks_automation/Laundry_TryCents'
+    ],
+    binaries=[],
+    datas=[
+        # Property files - using relative paths and correct target directory
+        ('property_files/calendar_api_properties.properties', 'property_files'),
+        ('property_files/calendar-automate-srvc-account-ref-file.json', 'property_files'),
+        ('property_files/google_desktop_oauth_client_contacts_api.json', 'property_files'),
+        ('property_files/token.pickle', 'property_files'),
+        # Laundry automation files
+        ('../Laundry_TryCents/laundry_automation.py', '.')
+    ],
+    hiddenimports=[
+        'google.auth',
+        'google.auth.transport.requests',
+        'google_auth_oauthlib.flow',
+        'googleapiclient.discovery',
+        'anthropic',
+        'jproperties',
+        'selenium',
+        'selenium.webdriver',
+        'selenium.webdriver.edge.options',
+        'selenium.webdriver.edge.service',
+        'selenium.webdriver.common.by',
+        'selenium.webdriver.support.ui',
+        'selenium.webdriver.support.expected_conditions',
+        'selenium.common.exceptions',
+        'oauth_setup'  # Add oauth_setup module
+    ],
     hookspath=[],
     hooksconfig={},
     runtime_hooks=[],
     excludes=[],
-    noarchive=False,
-    optimize=0,
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=block_cipher,
+    noarchive=False
 )
-pyz = PYZ(a.pure)
+
+# Add oauth_setup.py to the analysis
+a.datas += [('oauth_setup.py', 'oauth_setup.py', 'DATA')]
+
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
 
 exe = EXE(
     pyz,
     a.scripts,
     a.binaries,
+    a.zipfiles,
     a.datas,
     [],
     name='google_calendar_and_contacts_automate',
-    debug=False,
+    debug=True,
     bootloader_ignore_signals=False,
     strip=False,
     upx=True,
     upx_exclude=[],
     runtime_tmpdir=None,
-    console=False,
+    console=True,
     disable_windowed_traceback=False,
     argv_emulation=False,
     target_arch=None,
--- google_calendar_and_contacts_automate/oauth_setup.py
+++ google_calendar_and_contacts_automate/oauth_setup.py
@@ -5,6 +5,7 @@ import pickle
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from jproperties import Properties
 
 SCOPES = [
     'https://www.googleapis.com/auth/calendar',
@@ -17,40 +18,49 @@ SCOPES = [
 def get_oauth_credentials():
     """Gets valid user credentials from storage or initiates OAuth2 flow."""
     creds = None
-    
-    # Get the properties file path
+
+    # Get the directory where this script is located
     if getattr(sys, 'frozen', False):
         base_path = sys._MEIPASS
+        properties_dir = 'property_files'
     else:
-        base_path = os.path.abspath(".")
-        
-    # Read the properties file
-    from jproperties import Properties
+        # Get the directory where the script is located and go up one level
+        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        properties_dir = 'property_files'
+
+    # Read the properties file from the property_files directory
     properties = Properties()
-    properties_path = os.path.join(base_path, 'property_files', 'calendar_api_properties.properties')
-    
-    with open(properties_path, 'rb') as config_file:
-        properties.load(config_file)
-    
+    properties_path = os.path.join(base_path, properties_dir, 'calendar_api_properties.properties')
+
+    try:
+        with open(properties_path, 'rb') as config_file:
+            properties.load(config_file)
+    except FileNotFoundError:
+        print(f"Error: Properties file not found at {properties_path}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error reading properties file: {str(e)}")
+        sys.exit(1)
+
     # Get credentials from properties file
     email = properties.get('GOOGLE_EMAIL').data
     password = properties.get('GOOGLE_APP_PASSWORD').data
-    
+
     # Create or use existing client secrets file
-    client_secrets_file = os.path.join(base_path, 'property_files', 'google_desktop_oauth_client_contacts_api.json')
-    
+    client_secrets_file = os.path.join(base_path, properties_dir, 'google_desktop_oauth_client_contacts_api.json')
+
     # Verify client secrets file exists
     if not os.path.exists(client_secrets_file):
         print(f"Error: Client secrets file not found at {client_secrets_file}")
         sys.exit(1)
 
-    token_path = os.path.join(base_path, 'property_files', 'token.pickle')
-    
+    token_path = os.path.join(base_path, properties_dir, 'token.pickle')
+
     # Check if we have valid credentials in the token file
     if os.path.exists(token_path):
         with open(token_path, 'rb') as token:
             creds = pickle.load(token)
-    
+
     # If there are no (valid) credentials available, let the user log in
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -70,7 +80,7 @@ def get_oauth_credentials():
             flow = InstalledAppFlow.from_client_secrets_file(
                 client_secrets_file, SCOPES)
             creds = flow.run_local_server(port=0)
-        
+
         # Save the credentials for the next run
         with open(token_path, 'wb') as token:
             pickle.dump(creds, token)
